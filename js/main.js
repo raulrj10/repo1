d3644c1ac56b9b30ba2e5e92e3f99440386ac8c3
raulrj10 <raul.reyesjimenezico@gmail.com>
2020-03-10 22:07:18 -0600

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.toggle').click(function(){
+    $('.formulario').animate({
+        heigth: "toggle",
+        'padding-top':'toggle',
+        'padding-bottom':'toggle',
+        opacity:'toggle'
+
+
+    }, "slow");
+    
+});
